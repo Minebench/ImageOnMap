dc53243a72cd16a158931e7578053c9972c8d955
Max Lee <Phoenix616@users.noreply.github.com>
2015-07-10 20:11:41 +0200

    Cleanup, we don't really need to handle an unexisting directory

--- src/main/java/fr/moribus/imageonmap/ImageOnMap.java
+++ src/main/java/fr/moribus/imageonmap/ImageOnMap.java
@@ -75,13 +75,7 @@ public final class ImageOnMap extends JavaPlugin {
                         nbErr++;
                     }
                 }
-                // Do something with child
             }
-        } else {
-            // Handle the case where dir is not really a directory.
-            // Checking dir.isDirectory() above would not be sufficient
-            // to avoid race conditions with another process that deletes
-            // directories.
         }
         this.getLogger().info(nbMap + " maps were loaded");
         if (nbErr != 0) {
